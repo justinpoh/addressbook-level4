17f2ebc999af4ad3ef87269583faf22eb1006c6d
Justin Poh <e0031499@u.nus.edu>
2017-11-13 02:18:51 +0800

    Made changes to testing script as well as correcting a logic error.

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -206,14 +206,17 @@ public class Birthday {
      * Converts date in integer array into dd-mm-yyyy String format.
      */
     private static String convertToDefaultDateFormat(String date) {
-        String[] splitDate = getSplitDate(date);
-
+        String[] splitDate = date.split(BIRTHDAY_SPLIT_REGEX);
         StringBuilder builder = new StringBuilder();
-        builder.append(splitDate[DATE_DAY_INDEX]);
+        final int day = Integer.parseInt(splitDate[DATE_DAY_INDEX]);
+        final int month = Integer.parseInt(splitDate[DATE_MONTH_INDEX]);
+        final int year = Integer.parseInt(splitDate[DATE_YEAR_INDEX]);
+
+        builder.append(day);
         builder.append(BIRTHDAY_DASH_SEPARATOR);
-        builder.append(splitDate[DATE_MONTH_INDEX]);
+        builder.append(month);
         builder.append(BIRTHDAY_DASH_SEPARATOR);
-        builder.append(splitDate[DATE_YEAR_INDEX]);
+        builder.append(year);
         return builder.toString();
     }
 
